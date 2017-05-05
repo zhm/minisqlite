5d8ea0380f33a310a4b0adf47bb950d0d342dcd7
Zac McCormick <zac.mccormick@gmail.com>
2017-05-05 10:59:41 -0400

    Fix script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,26 @@ export CC=clang
 export CXX=clang++
 export npm_config_clang=1
 
+if [ $TRAVIS_OS_NAME != "linux" ]; then
+  git clone https://github.com/creationix/nvm.git
+  ./.nvm
+  source ./.nvm/nvm.sh
+fi
+
+if [ "$RUNTIME" == "electron" ]; then
+  echo "Building electron $TARGET"
+  export npm_config_target=$TARGET
+  export npm_config_arch=$TARGET_ARCH
+  export npm_config_target_arch=$TARGET_ARCH
+  export npm_config_disturl=https://atom.io/download/electron
+  export npm_config_runtime=electron
+  export npm_config_build_from_source=true
+fi
+
+nvm install $NODE_VERSION
+
+if [ -z "$TARGET" ]; then
+  export TARGET=$(node -v | sed -e '1s/^.//')
+fi
+
 HOME=~/.electron-gyp npm install --build-from-source
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,2 +1,6 @@
+if [ -z "$TARGET" ]; then
+  export TARGET=$(node -v | sed -e '1s/^.//')
+fi
+
 ./node_modules/.bin/node-pre-gyp package --runtime=$RUNTIME --target=$TARGET
 ./node_modules/.bin/node-pre-gyp publish --runtime=$RUNTIME --target=$TARGET
